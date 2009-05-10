88adcd35f6a931b57d092616eeeda2a5ef368011
suztomo <suztomo@gmail.com>
2009-05-10 21:06:07 +0900

    module parameter receiving

--- module_param/module_param.c
+++ module_param/module_param.c
@@ -31,6 +31,25 @@ MODULE_LICENSE("GPL");
 
 
 /* 
+ * UID of the initial processes owner.
+
+ */
+static int initial_uid;
+module_param(initial_uid, int, 0644);
+
+
+/*
+  Gets a mapping of (process id -> node id)
+ */
+int pid_array[10];
+int pid_array_count;
+module_param_array(pid_array, int, &pid_array_count, 0000);
+
+int node_array[10];
+int node_array_count;
+module_param_array(node_array, int, &node_array_count, 0000);
+
+/* 
  * Initialize the module - replace the system call 
  */
 int init_module()
@@ -38,19 +57,22 @@ int init_module()
   int i;
   struct task_struct *task = &init_task;
 
-  do {
-    if (task->uid == initial_uid) {
-      printk(KERN_INFO "*** %s [%d] parent %s\n",
-             task->comm, task->trace_nid, task->parent->comm);
-      task->trace_nid != ;
-    } else {
+  printk(KERN_INFO "pid_array %d\n", pid_array_count);
 
-    }
+  if (!pid_array_count) {
+    printk(KERN_INFO "specify node_array and pid_array\n");
+    return -1;
+  }
 
-    if (task->trace_nid != 123) {
-      printk(KERN_INFO "Fucking trace_nid %d\n", task->trace_nid);
-    }
-  } while ((task = next_task(task)) != &init_task);
+  if (pid_array_count != node_array_count) {
+    printk(KERN_INFO "Array size mismatch: pid_array and node_array\n");
+    return -1;
+  }
+
+  for (i=0; i<pid_array_count; ++i) {
+    printk(KERN_INFO "*** %d : %d\n", pid_array[i], node_array[i]);
+  }
+
   return 0;
 }
 
